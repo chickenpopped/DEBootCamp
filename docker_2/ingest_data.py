4016420f8e6b58a551ef509d0dc7b7426aa3836b
Kevin Ye <kevinye@gmail.com>
2024-11-30 15:22:23 -0800

    docker module done

--- docker_2/ingest_data.py
+++ docker_2/ingest_data.py
@@ -5,23 +5,36 @@ from sqlalchemy import create_engine
 from time import time
 import argparse
 
-
 def main(params):
     user = params.user
     password = params.password        
     host = params.host
     port = params.port
     db = params.db
-    table_name = params.table_name
-    url = params.url
-    csv_name = 'output.csv'
+    data_table = params.data_table
+    zone_table = params.zone_table
+    data_url = params.data_url
+    zone_url = params.zone_url
     
-    if url.endswith('.csv.gz'):
+    if zone_url.endswith('.csv.gz'):
+        zone_name = 'zones.csv.gz'
+    else:
+        zone_name = 'zones.csv'
+    
+    os.system(f"wget {zone_url} -O {zone_name}")
+    
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    zone_df = pd.read_csv(zone_name)
+    zone_df.to_sql(name=zone_table, con=engine)
+    
+    
+    if data_url.endswith('.csv.gz'):
         csv_name = 'output.csv.gz'
     else:
         csv_name = 'output.csv'
     
-    os.system(f"wget {url} -O {csv_name}")
+    os.system(f"wget {data_url} -O {csv_name}")
     
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
@@ -30,13 +43,18 @@ def main(params):
     # iter through first csv
 
     while True:
-        t_start = time()
-        df = next(df_iter)
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        t_end = time()
-        df.to_sql(name = table_name, con = engine, if_exists='append')
-        print('chunk inserted ..., took %.3f second' % (t_end - t_start))
+        try:
+            t_start = time()
+            df = next(df_iter)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            t_end = time()
+            df.to_sql(name = data_table, con = engine, if_exists='append')
+            print('chunk inserted ..., took %.3f second' % (t_end - t_start))
+            
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
     
    
 if __name__ == '__main__':
@@ -50,9 +68,13 @@ if __name__ == '__main__':
     parser.add_argument('--host', help = 'host for postgres')
     parser.add_argument('--port', help = 'port for postgres')
     parser.add_argument('--db', help = 'database name for postgres')
-    parser.add_argument('--table_name', help = 'table name for postgres')
-    parser.add_argument('--url', help = 'url of csv for postgres')
+    parser.add_argument('--data_table', help = 'data table name for postgres')
+    parser.add_argument('--zone_table', help = 'zone table name for postgres')
+    parser.add_argument('--data_url', help = 'url of csv for postgres')
+    parser.add_argument('--zone_url', help = 'url of csv for postgres')
     
     args = parser.parse_args()
+
+    print("Parsed arguments:", args)
     
     main(args)
