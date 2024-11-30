98558636adc366893d72ab81bf1500f5f191c092
Kevin Ye <kevinye@gmail.com>
2024-11-30 11:57:49 -0800

    shtuff

--- docker_2/ingest_data.py
+++ docker_2/ingest_data.py
@@ -14,19 +14,22 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    
     csv_name = 'output.csv'
     
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+    
     os.system(f"wget {url} -O {csv_name}")
+    
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(csv_name, iterator = True, chunksize=100000, compression = 'gzip')
     
-
-    
+    # iter through first csv
 
     while True:
-
         t_start = time()
         df = next(df_iter)
         df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
@@ -35,7 +38,7 @@ def main(params):
         df.to_sql(name = table_name, con = engine, if_exists='append')
         print('chunk inserted ..., took %.3f second' % (t_end - t_start))
     
-    
+   
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = 'Ingest CSV to postgres')
